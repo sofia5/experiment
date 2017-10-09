f4d117b0bc74917b7a255dac7cc05957a1e2b1d9
fannychan <fannt@kth.se>
2017-10-09 23:15:29 +0200

    Working linked list

--- main.c
+++ main.c
@@ -11,15 +11,67 @@ struct Element* head;
 struct Element* tail;
 
 struct Element* GetNewElement(){
-    int random = rand() % 41;
+    int random;
+    random = rand() % 41;
     struct Element* newElement = (struct Element*)malloc(sizeof(struct Element));
     newElement->prio = random;
     newElement->next = NULL;
     newElement->prev = NULL;
 }
 
-void Insert(int numberOfElements){
-    for (int i = 0; i <= numberOfElements; i++) {
+//Inserts a Node at tail of Doubly linked list
+void InsertFromTail(struct Element* newElement) {
+    struct Element* temp = tail;
+    while ( temp != NULL && temp->prio < newElement->prio ) {
+        temp = temp->prev;
+    }
+    if (temp == NULL) {
+        head->prev = newElement;
+        newElement->next = head;
+        head = newElement;
+    }
+    else {
+        struct Element* nextElement = temp->next;
+        temp->next = newElement;
+        newElement->prev = temp;
+
+        if ( nextElement == NULL){
+            tail = newElement;
+        }
+        else {
+            nextElement->prev = newElement;
+            newElement->next = nextElement;
+        }
+    }
+}
+
+void InsertFromHead (struct Element* newElement) {
+    struct Element* temp = head;
+    while ( temp != NULL && temp->prio > newElement->prio) {
+        temp = temp->next;
+    }
+    if (temp == NULL) {
+        tail->next = newElement;
+        newElement->prev = tail;
+        tail = newElement;
+    }
+    else {
+        struct Element* prevElement = temp->prev;
+        temp->prev = newElement;
+        newElement->next = temp;
+
+        if(prevElement == NULL){
+            head = newElement;
+        }
+        else {
+            prevElement->next = newElement;
+            newElement->prev = prevElement;
+        }
+    }
+}
+
+void InsertElements(int numberOfElements){
+    for (int i = 0; i < numberOfElements; i++) {
         struct Element *newElement = GetNewElement();
         if (head == NULL) {
             head = newElement;
@@ -32,59 +84,21 @@ void Insert(int numberOfElements){
     }
 }
 
-//Inserts a Node at head of doubly linked list
-void InsertFromHead(struct Element* newElement) {
-    struct Element* temp = head;
-    while (temp->next != NULL) {
-        if(newElement->prio > temp->prio){
-            newElement->prev = temp->prev;
-            newElement->next = temp;
-            temp->prev = newElement;
-            return;
-        }
-        else{
-            temp = temp->next;
-        }
-    }
-    newElement->prev = temp->prev;
-    newElement->next = temp;
-    temp->prev = newElement;
-    tail = newElement;
-}
-
-//Inserts a Node at tail of Doubly linked list
-void InsertFromTail(struct Element* newElement) {
-    struct Element *temp = tail;
-    while (temp->prev != NULL) {
-        if (newElement->prio < temp->prio) {
-            newElement->next = temp->next;
-            newElement->prev = temp;
-            temp->next = newElement;
-            return;
-        } else {
-            temp = temp->prev;
-        }
-    }
-    newElement->next = temp->next;
-    newElement->prev = temp;
-    temp->next = newElement;
-    tail = newElement;
-}
-
-int main() {
-    Insert(4);
-    printf("Hello, World!\n");
-    PrintAll();
-    return 0;
-}
-
 //Prints all the elements in linked list in forward traversal order
 void PrintAll() {
     struct Element* temp = head;
     printf("Forward: ");
     while(temp != NULL) {
         printf("%d ",temp->prio);
-        temp = temp->prev;
+        temp = temp->next;
     }
     printf("\n");
 }
+
+int main() {
+    InsertElements(51);
+    printf("Hello, World!\n");
+    PrintAll();
+    return 0;
+}
+
